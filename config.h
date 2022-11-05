ae23e8d099c888db9b88c6acc288c4cf066f2edd
TheDirams <thedirams@gmail.com>
2022-11-05 23:08:35 +0100

    some cleaning

--- config.h
+++ config.h
@@ -3,18 +3,18 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"",	"sb-pacpackages",	   0,	      8},
 	{"",	"sb-forecast",	       18000,	      5},
-      /*{"",	"sb-nettraf",              1,         16},*/
-      /*{"",    "sb-cpubars",              1,         16},*/
-        {"",    "sb-memory",	          10,	      14},
-	{"",	"sb-cpu",		  10,	      18},
-	{"",	"sb-volume",	           0,         10},
-	{"",	"sb-clock",	          60,          1},
-	{"",	"sb-internet",	           5,          4},
-	//{"",	"sb-battery",	           5,          3},
+  /*{"",	"sb-nettraf",              1,         16},*/
+  /*{"",    "sb-cpubars",              1,         16},*/
+  {"",    "sb-memory",	         10,	       14},
+	{"",	"sb-cpu",		             10,	       18},
+	{"",	"sb-volume",	            0,         10},
+	{"",	"sb-clock",	             60,          1},
+	{"",	"sb-internet",	          5,          4},
+	//{"",	"sb-battery",	          5,          3},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
