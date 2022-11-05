fd205228d9beb36bba027cf61e3f72e31d6f368a
TheDirams <thedirams@gmail.com>
2022-11-05 23:48:27 +0100

    Cleaning some code again

--- config.h
+++ config.h
@@ -1,6 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
+
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+
+  /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
   {"",	"sb-pacpackages",	         0,	      8},
 	{"",	"sb-forecast",	       18000,	      5},
@@ -15,8 +19,12 @@ static const Block blocks[] = {
 
 };
 
+
+
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char *delim = " ";
+
+
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
