f65822d6dcd30b69cfb7ff8e280e00ff5b484402
TheDirams <thedirams@gmail.com>
2022-11-05 23:30:31 +0100

    some cleaning

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"sb-pacpackages",	   0,	      8},
+	{"",	"sb-pacpackages",	         0,	      8},
 	{"",	"sb-forecast",	       18000,	      5},
-  /*{"",	"sb-nettraf",              1,         16},*/
-  /*{"",    "sb-cpubars",              1,         16},*/
+  //{"",	"sb-nettraf",             1,         16},
+  //{"",    "sb-cpubars",           1,         16},
   {"",    "sb-memory",	         10,	       14},
 	{"",	"sb-cpu",		             10,	       18},
 	{"",	"sb-volume",	            0,         10},
 	{"",	"sb-clock",	             60,          1},
 	{"",	"sb-internet",	          5,          4},
-	//{"",	"sb-battery",	          5,          3},
+	//{"",	"sb-battery",	            5,          3},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
